7c8b7174eb40982f020e86e291302d5872745376
Andrii <123946757+Kochey@users.noreply.github.com>
2023-01-30 13:46:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector('.btn').onclick = function() {
+    console.log(123);
+    let header = document.querySelector('header');
+    header.style.backgroundImage = 'url("img/BG2.png")';
+}
+let btn2 = document.querySelector('.btn2').onclick = function() {
+    console.log(123);
+    let header = document.querySelector('header');
+    header.style.backgroundImage = 'url("img/BG.png")';
+}
